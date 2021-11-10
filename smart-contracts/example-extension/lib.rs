41bdf544e50223a98fec4b5d76db6ff9b69ab0e8
Justin <--global>
2021-11-10 09:01:44 -0500

    clarify smart contract fn naming and comments

--- smart-contracts/example-extension/lib.rs
+++ smart-contracts/example-extension/lib.rs
@@ -10,47 +10,29 @@ use ink_lang as ink;
 /// Here we define the operations to interact with the Substrate runtime.
 #[ink::chain_extension]
 pub trait FetchRandom {
-	type ErrorCode = RandomReadErr;
-
-	/// Note: this gives the operation a corresponding `func_id` (1101 in this case),
-	/// and the chain-side chain extension will get the `func_id` to do further operations.
-	#[ink(extension = 1101, returns_result = false)]
-	fn fetch_random() -> [u8; 32];
-
-	// #[ink(extension = 1102, returns_result = true)]
-	// fn call_extrinsic(val: u8);
-
-	// #[ink(extension = 2]
-	// fn read_small(key: &[u8]) -> Result<(&[u8]), RandomReadErr>;
-
-	// #[ink(extension = 2)]
-    // // fn read_small(key: &[u8]) -> Result<(u32, [u8; 32]), RandomReadErr>;
-    // fn send_to_pallet(key: &[u8]) -> Result<(), RandomReadErr>;
-
+	type ErrorCode = RuntimeCallErr;
 
 	#[ink(extension = 2)]
-	fn read(key: &u8) -> Result<(u32, [u8; 32]), RandomReadErr>;
+	fn call_pallet(key: &u8) -> Result<(u32, [u8; 32]), RuntimeCallErr>;
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-pub enum RandomReadErr {
-	FailGetRandomSource,
-	FailToCallRuntime
+pub enum RuntimeCallErr {
+	FailToCallRuntime,
 }
 
-impl From<scale::Error> for RandomReadErr {
+impl From<scale::Error> for RuntimeCallErr {
 	fn from(_: scale::Error) -> Self {
 		panic!("encountered unexpected invalid SCALE encoding")
 	}
 }
 
-impl ink_env::chain_extension::FromStatusCode for RandomReadErr {
+impl ink_env::chain_extension::FromStatusCode for RuntimeCallErr {
 	fn from_status_code(status_code: u32) -> Result<(), Self> {
 		match status_code {
 			0 => Ok(()),
-			1 => Err(Self::FailGetRandomSource),
-			2 => Err(Self::FailToCallRuntime),
+			1 => Err(Self::FailToCallRuntime),
 			_ => panic!("encountered unknown status code"),
 		}
 	}
@@ -75,7 +57,7 @@ impl Environment for CustomEnvironment {
 
 #[ink::contract(env = crate::CustomEnvironment)]
 mod rand_extension {
-	use super::RandomReadErr;
+	use super::RuntimeCallErr;
 
 	/// Defines the storage of our contract.
 	///
@@ -104,9 +86,9 @@ mod rand_extension {
 			Self { value: Default::default(), stored_number: Default::default() }
 		}
 
-		/// Update a value given by argument
+		/// An example smart contract function demonstrating interactions originating in a custom pallet
 		#[ink(message)]
-		pub fn call_from_pallet(&mut self, value: u32) -> Result<(), RandomReadErr> {
+		pub fn call_from_pallet(&mut self, value: u32) -> Result<(), RuntimeCallErr> {
 			self.stored_number = value;
 			self.env().emit_event(NumUpdated { num: value });
 			Ok(())
@@ -114,9 +96,8 @@ mod rand_extension {
 
 		/// Update a value given by argument
 		#[ink(message)]
-		// pub fn send_to_pallet(&mut self, value: Vec<u8>) -> Result<
-		pub fn send_to_pallet(&mut self, value: u8) -> Result<(),RandomReadErr> {
-			self.env().extension().read(&value)?;
+		pub fn send_to_pallet(&mut self, value: u8) -> Result<(),RuntimeCallErr> {
+			self.env().extension().call_pallet(&value)?;
 			Ok(())
 		}
 
@@ -143,12 +124,11 @@ mod rand_extension {
 
 		#[ink::test]
 		fn chain_extension_works() {
-			// given
 			struct MockedExtension;
 			impl ink_env::test::ChainExtension for MockedExtension {
 				/// The static function id of the chain extension.
 				fn func_id(&self) -> u32 {
-					1101
+					2
 				}
 
 				/// The chain extension is called with the given input.
@@ -156,7 +136,7 @@ mod rand_extension {
 				/// Returns an error code and may fill the `output` buffer with a
 				/// SCALE encoded result. The error code is taken from the
 				/// `ink_env::chain_extension::FromStatusCode` implementation for
-				/// `RandomReadErr`.
+				/// `RuntimeCallErr`.
 				fn call(&mut self, _input: &[u8], output: &mut Vec<u8>) -> u32 {
 					let ret: [u8; 32] = [1; 32];
 					scale::Encode::encode_to(&ret, output);
